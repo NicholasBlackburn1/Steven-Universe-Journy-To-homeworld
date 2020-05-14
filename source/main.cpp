cd6586c7a00b496b718de13e0e2457af99811b18
nicholasblackburn1 <nickblakcburn02@gmail.com>
2020-05-14 19:23:51 -0400

    got image working

--- source/main.cpp
+++ source/main.cpp
@@ -1,29 +1,27 @@
 
-#include <citro2d.h>
 #include <m3dia.hpp>
-#include "m3d/graphics/boundingBox.hpp"
-#include "m3d/graphics/color.hpp"
-#include "m3d/graphics/drawable.hpp"
-#include "m3d/graphics/texture.hpp"
-#include "m3d/graphics/vertex.hpp"
+
 
 
 int main() {
     // instance of the applet
     m3d::Applet app;
-    m3d::Screen screen;
-    m3d::Sprite title;
+    m3d::Screen scr;
+    m3d::Sprite title("romfs:/gfx/title.t3x",0);
     
-    consoleInit(GFX_TOP, NULL);
-
     // standard C printf()
-    printf("Hello, Steven Unierse Journey to homeworld is loading \n");
+    printf("Hello, Steven Unierse Journey to homeworld is loading\n");
         
     // app's main loop
     while(app.isRunning()) {
         
-        //title.setXPosition(s)
-        
+
+        if (m3d::buttons::buttonPressed(m3d::buttons::Start)) {
+            
+            scr.drawTop(title);
+            scr.render();
+        }
+    
         
         if (m3d::buttons::buttonPressed(m3d::buttons::B)) {
             app.exit();
